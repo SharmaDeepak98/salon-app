1e38d2cf92434b58f6f2ee9643b333cc8b4b312c
Deepak Sharma <076bct029@ioepc.edu.np>
2024-12-26 17:23:42 +0545

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust based on your project structure
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        robotoCondensed: ['"Roboto Condensed"', 'sans-serif'],
+        instrumentSerif: ['"Instrument Serif"', 'serif'],
+        dancingScript: ['"Dancing Script"', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
